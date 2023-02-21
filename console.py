a802ee8dacda2cf7b1a919c530bb244796edcbab
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-21 07:15:19 -0800

    Added do_create to console

--- console.py
+++ console.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """Console for AirBnB"""
 import cmd
+import sys
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
     """class of the interpreter"""
     prompt = '(hbnb)'
+    class_list = ["BaseModel"]
 
     def do_quit(self, arg):
         """quits the console"""
@@ -16,6 +20,18 @@ class HBNBCommand(cmd.Cmd):
         exit()
 
     def emptyline(self):
+        """handles empty lines"""
         pass
+
+    def do_create(self, arg):
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg not in self.class_list:
+            print("** class doesn't exist **")
+        else:
+            newInstance = eval(arg)()
+            newInstance.save()
+            print(newInstance.id)
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
