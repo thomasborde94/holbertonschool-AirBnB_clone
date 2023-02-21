3ab5498bc3b439d73fe70c06c397de1a0aa88235
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-21 06:22:39 -0800

    Added first implementation of the console

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Console for AirBnB"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class of the interpreter"""
+    prompt = '(hbnb)'
+    file = None
+
+    def do_quit(self, arg):
+        """quits the console"""
+        exit()
+    def do_EOF(self, arg):
+        """quits the console"""
+        print()
+        exit()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
