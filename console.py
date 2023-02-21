1c9ab03d0e6c3258ccfa045411246f38582a4340
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-21 17:37:03 +0100

    command do_show added

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ import cmd
 import sys
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """class of the interpreter"""
@@ -24,6 +24,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
+        """create a new instance"""
         if len(arg) == 0:
             print("** class name missing **")
         elif arg not in self.class_list:
@@ -32,6 +33,28 @@ class HBNBCommand(cmd.Cmd):
             newInstance = eval(arg)()
             newInstance.save()
             print(newInstance.id)
+
+    def do_show(self, arg):
+        token = arg.split(" ")
+        dict_obj = storage.all()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+
+        elif token[0] not in self.class_list:
+            print("** class doesn't exist **")
+
+        elif len(token) < 2:
+                print("** instance id missing **")
+
+        else:
+            instance_key = "{}.{}".format(token[0], token[1])
+            
+            if instance_key not in dict_obj.keys():
+                print("** no instance found **")
+
+            else:
+                print(dict_obj[instance_key])
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
