e95b97a946213228220bd5e173237a522fd143ef
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-22 00:55:49 -0800

    First implementation of class User

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """Defines a class FileStorage."""
 import json
 from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage():
---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines the class User"""
+from models.base_model import BaseModel
+
+
+
+class User(BaseModel):
+    """represents a user, inherits from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
