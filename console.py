0639c2f3fe22bcbabd7eb96216588d74f57576d5
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-22 11:49:58 +0100

    command destroy, all, update added

--- console.py
+++ console.py
@@ -5,11 +5,19 @@ import sys
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     """class of the interpreter"""
     prompt = '(hbnb)'
-    class_list = ["BaseModel"]
+    class_list = ["BaseModel", "User", "State", "City",
+                  "Amenity", "Place", "Review"]
 
     def do_quit(self, arg):
         """quits the console"""
@@ -55,6 +63,75 @@ class HBNBCommand(cmd.Cmd):
 
             else:
                 print(dict_obj[instance_key])
+
+    def do_destroy(self, arg):
+        token = arg.split(" ")
+        dict_obj = storage.all()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+
+        elif token[0] not in self.class_list:
+            print("** class doesn't exist **")
+
+        elif len(token) < 2:
+                print("** instance id missing **")
+
+        else:
+            instance_key = "{}.{}".format(token[0], token[1])
+
+            if instance_key not in dict_obj.keys():
+                print("** no instance found **")
+
+            else:
+                del dict_obj[instance_key]
+                storage.save()
+
+    def do_all(self, arg):
+        dict_obj = storage.all()
+        list_obj = []
+
+        if len(arg) == 0:
+            for keys, values in dict_obj.items():
+                list_obj.append(str(values))
+            print(list_obj)
+
+        elif arg in self.class_list:
+            for keys, values in dict_obj.items():
+                if values.__class__.__name__ == arg:
+                    list_obj.append(str(values))
+            print(list_obj)
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        token = arg.split(" ")
+        dict_obj = storage.all()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+
+        elif token[0] not in self.class_list:
+            print("** class doesn't exist **")
+
+        elif len(token) < 2:
+                print("** instance id missing **")
+
+        else:
+            instance_key = "{}.{}".format(token[0], token[1])
+            
+            if instance_key not in dict_obj.keys():
+                print("** no instance found **")
+
+            elif len(token) < 3:
+                print("** attribute name missing **")
+
+            elif len(token) < 4:
+                print("** value missing **")
+
+            else:
+                setattr(dict_obj[instance_key], token[2], token[3])
+                storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
